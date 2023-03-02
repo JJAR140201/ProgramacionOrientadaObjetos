ad0796bd4658d338465aed10508c916ffc0e5523
JJAR140201 <avilesroajuanjose@gmail.com>
2023-03-02 15:52:42 -0500

    Concepto de abstraccion

---
+++ index.ts
@@ -0,0 +1,16 @@
+//Principio de abstraccion
+
+class Smartphone {
+    color:String;
+    brand:String;
+
+    constructor(color: String, brand: String){
+        color:String;
+        brand:String;
+    }
+    
+    makeAPhoneCall(): void {
+    }
+}
+
+let obj = new Smartphone('White', 'Iphone');
