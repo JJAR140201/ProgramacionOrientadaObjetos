c8931e372b0c45fc557ab3cd365f4d9c21008f5c
JJAR140201 <avilesroajuanjose@gmail.com>
2023-03-02 16:15:07 -0500

    Principio de Encapsulamiento

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
 //Principio de abstraccion
 
 class Smartphone {
-    color:String;
-    brand:String;
+    private color:String;
+    private brand:String;
+    private _battery: number;
 
     constructor(color: String, brand: String){
-        color:String;
-        brand:String;
+        this.color = color;
+        this.brand = brand;
+        this._battery = 100;
     }
     
     makeAPhoneCall(): void {
+        if(this._battery === 0){
+            throw "No cuenta con la bateria necesaria"
+        }
+        this._battery -= 10;
+    }
+
+    get battery() : number{
+        return this._battery;
+    }
+
+    recharge(){
+        this._battery = 100;
     }
 }
 //Programacion Orientada a Objetos
 let obj = new Smartphone('White', 'Iphone');
 obj.makeAPhoneCall();
-obj.color = 'Red'
-obj.brand = 'Android'
+obj.battery;
